e568b31c88ade7bb515c01c1a3970baa76ce95e7
Mark Allen <markallenarchviz@gmail.com>
2023-04-10 05:30:02 -0300

    Requisito 12

--- src/services/post.service.js
+++ src/services/post.service.js
@@ -1,21 +1,15 @@
 const { BlogPost } = require('../models');
 const { PostCategory } = require('../models');
 
-let date = new Date();
-date = date.getUTCFullYear() + '-' +
-    ('00' + (date.getUTCMonth()+1)).slice(-2) + '-' +
-    ('00' + date.getUTCDate()).slice(-2) + ' ' + 
-    ('00' + date.getUTCHours()).slice(-2) + ':' + 
-    ('00' + date.getUTCMinutes()).slice(-2) + ':' + 
-    ('00' + date.getUTCSeconds()).slice(-2);
+const date = new Date().toISOString().slice(0, 19).replace('T', ' ');
 
 const checks = (title, content, categoryIds) => {
     const msg = { status: 400, message: 'Some required fields are missing' };
-    const msg2 = { status: 400, message: 'one or more \"categoryIds\" not found' };
+    const msg2 = { status: 400, message: 'one or more "categoryIds" not found' };
     if (!title || !content || categoryIds.length === 0) throw msg;
     categoryIds.forEach((el) => {
         if (el > 2) throw msg2;
-    })
+    });
 };
 
 const addPost = async (title, content, categoryIds, userId) => {
